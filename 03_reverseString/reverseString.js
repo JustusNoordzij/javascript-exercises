f67ed26c264b638b5c69d0e6c4336d229b517636
Justus <justusnoordzij@hotmail.com>
2022-08-22 09:31:06 +0200

    added solutions

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    splitString = str.split("");
+    reverseArray = splitString.reverse();
+    joinArray = reverseArray.join("");
 
+
+// eerst splitten zodat alle char los zijn split()
+// dan de char volgorde omdraaien met reverse()
+// dan joinen met join()
+// dan returnen
+
+return joinArray;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,13 @@
-const ftoc = function() {
+const ftoc = function(temp) {
+  celsius = (temp - 32) * (5/9);
+  return rounded = Math.round(celsius * 10) / 10;
+  
 
 };
 
-const ctof = function() {
+const ctof = function(temp) {
+  fahrenheit = (temp * (9/5) + 32);
+  return rounded = Math.round(fahrenheit * 10) / 10;
 
 };
 
